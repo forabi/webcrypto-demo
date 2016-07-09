6a9e0ea00d66f3e8e5c36902b3e256b3baa1462c
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-07-09 09:02:50 +0300

    Use AES-GCM instead of AES-CTR

--- src/crypto.js
+++ src/crypto.js
@@ -3,10 +3,10 @@ export const format = 'jwk';
 export const algorithms = {
   dh: {
     name: 'ECDH',
-    namedCurve: 'P-256', // can be "P-256", "P-384", or "P-521"
+    namedCurve: 'P-256', // can be 'P-256', 'P-384', or 'P-521'
   },
   aes: {
-    name: 'AES-CTR',
+    name: 'AES-GCM',
     length: 256, // can be  128, 192, or 256
   },
 };
